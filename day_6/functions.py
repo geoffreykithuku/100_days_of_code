fd525ca97383908cac4bced977937c88aeb1253a
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-04-20 23:46:21 +0300

    day 7

--- day_6/functions.py
+++ day_6/functions.py
@@ -1,5 +1,16 @@
 def greeting():
     print("Hello")
     print("World")
-    
+
+
+# function call
 greeting()
+
+
+# function with arguments
+
+def my_function(fname, lname):
+    print(fname + " " + lname)
+
+
+my_function("Emil", "Refsnes")
