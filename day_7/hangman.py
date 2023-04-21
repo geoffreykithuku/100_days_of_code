7e016d88d67ba5b09ecf6374b202a34f9af36023
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-04-21 18:44:45 +0300

    day 7 hangman

---
+++ day_7/hangman.py
@@ -0,0 +1,43 @@
+import random
+# name = input("What is your name: ")
+
+# print("*************************************")
+# print(f"Hello {name}, welcome to hangman game")
+# print("*************************************")
+# print("*************************************")
+
+
+
+
+word_dict = ["bee", "ball", "egg", "sun", "son", "moon", "goat", "love", "true", "pen", "pig"]
+
+random_word = random.choice(word_dict)
+lives = len(random_word)
+blank = []
+end_of_game = False
+
+
+for _ in range(len(random_word)):
+    blank += "_"
+    
+print("The correct word is " + random_word)
+while not end_of_game:
+    guess = input("Guess a letter ").lower()
+
+    for position in range(len(random_word)):
+        letter = random_word[position]
+        
+        if letter == guess:
+            blank[position] = letter
+    
+    if guess not in random_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+            
+    print(f"{' '.join(blank)}")
+
+    if "_" not in blank:
+        end_of_game = True
+        print("You win")
